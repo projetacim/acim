57e4ef40fa527116565dcd64396ae172acc86f11
BIJOU ET JAC TOUATI <jjmnbtouati@gmail.com>
2025-10-17 19:19:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4992842764-b1cd9",
+  "appId": "1:288234719555:web:e7e656edca37c10d7747f6",
+  "apiKey": "AIzaSyBtk_6WsA6omnNxNeiZ9yBpzjlNpD4NzOE",
+  "authDomain": "studio-4992842764-b1cd9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "288234719555"
+};
