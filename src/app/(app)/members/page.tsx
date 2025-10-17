9ca8c3a1d653442169f7b03fbb3d3fa2b2a4778a
BIJOU ET JAC TOUATI <jjmnbtouati@gmail.com>
2025-10-17 21:19:24 +0000

    a supprimer  (_for element h1_)

--- src/app/(app)/members/page.tsx
+++ src/app/(app)/members/page.tsx
@@ -5,7 +5,6 @@ import { MembersTable } from './components/members-table';
 export default function MembersPage() {
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold tracking-tight">Member Management</h1>
       <MembersTable />
     </div>
   );
